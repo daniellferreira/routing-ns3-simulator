d2fc28f1ef80b6a5428e7cb3f2c97c6081521b4c
vmatter <vitorkmatter@gmail.com>
2022-04-09 22:47:42 -0300

    Finished comparing the ospf version with the rip.

--- tp1/ospf_tp1.cc
+++ tp1/ospf_tp1.cc
@@ -3,8 +3,6 @@
 //  HostT ---------- RouterA ---------- RouterB ---------- RouterC ---------- HostR
 //
 
-// - Tracing of queues and packet receptions to file "dynamic-global-routing.tr"
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -24,11 +22,12 @@ using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("DynamicGlobalRoutingExample");
 
-int 
-main (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
   // Adicionado
   bool verbose = true;
+  double simulationTime = 131.0; //seconds
+  std::string transportProt = "Udp";
 
   // The below value configures the default behavior of global routing.
   // By default, it is disabled.  To respond to interface events, set to true
@@ -68,11 +67,11 @@ main (int argc, char *argv[])
   NodeContainer net4 (c, dst);
   NodeContainer routers (a, b, c);
   NodeContainer nodes (src, dst);
+  NS_LOG_INFO ("End create nodes.");
 
   NS_LOG_INFO ("Create channels.");
   // Enlaces usam CSMA para controle de acesso ao meio
   // É como se tivesse ligado os cabos entre as máquinas
-
   CsmaHelper csma;
   csma.SetChannelAttribute ("DataRate", DataRateValue (5000000));
   csma.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (2)));
@@ -82,6 +81,8 @@ main (int argc, char *argv[])
   NetDeviceContainer ndc4 = csma.Install (net4);
 
   NS_LOG_INFO ("Create IPv4 and routing.");
+
+  // Na implementacao do Global routing nao tem essa parte do RoutingHelper.
   
   InternetStackHelper internet;
   internet.SetIpv6StackInstall (false); //sem Ipv6
@@ -93,6 +94,10 @@ main (int argc, char *argv[])
 
   // Later, we add IP addresses.
   NS_LOG_WARN ("Assign IP Addresses.");
+  Ipv4AddressHelper ipv4;
+
+  //dando endereços de rede para os enlaces
+  //como se tivesse usado DHCP para os enlaces entre os nodos
 
   ipv4.SetBase (Ipv4Address ("10.0.0.0"), Ipv4Mask ("255.255.255.0"));
   Ipv4InterfaceContainer iic1 = ipv4.Assign (ndc1);
@@ -103,111 +108,72 @@ main (int argc, char *argv[])
   ipv4.SetBase (Ipv4Address ("10.0.2.0"), Ipv4Mask ("255.255.255.0"));
   Ipv4InterfaceContainer iic3 = ipv4.Assign (ndc3);
 
-  ipv4.SetBase (Ipv4Address ("10.0.2.1"), Ipv4Mask ("255.255.255.0"));
-  Ipv4InterfaceContainer iic3 = ipv4.Assign (ndc4);
-  
-  // Parei aqui
+  ipv4.SetBase (Ipv4Address ("10.0.3.0"), Ipv4Mask ("255.255.255.0"));
+  Ipv4InterfaceContainer iic4 = ipv4.Assign (ndc4);
 
   // Create router nodes, initialize routing database and set up the routing
   // tables in the nodes.
   Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+  // Creio que isso fico no lugar do Ptr<Ipv4StaticRouting> staticRouting; 
+  // no caso do rip
 
-  // Create the OnOff application to send UDP datagrams of size
-  // 210 bytes at a rate of 448 Kb/s
   NS_LOG_WARN ("Create Applications.");
-  uint16_t port = 9;   // Discard port (RFC 863)
-  OnOffHelper onoff ("ns3::UdpSocketFactory",
-                     InetSocketAddress (i5i6.GetAddress (1), port));
-  onoff.SetConstantRate (DataRate ("2kbps"));
-  onoff.SetAttribute ("PacketSize", UintegerValue (50));
-
-  ApplicationContainer apps = onoff.Install (c.Get (1));
+  //
+  // Create a UdpEchoServer application on node one.
+  //
+  uint16_t port = 9;  // well-known echo port number
+  UdpEchoServerHelper server (port);
+  ApplicationContainer apps = server.Install (dst);
   apps.Start (Seconds (1.0));
-  apps.Stop (Seconds (10.0));
+  apps.Stop (Seconds (simulationTime - 10.0));
 
-  // Create a second OnOff application to send UDP datagrams of size
-  // 210 bytes at a rate of 448 Kb/s
-  OnOffHelper onoff2 ("ns3::UdpSocketFactory",
-                      InetSocketAddress (i1i6.GetAddress (1), port));
-  onoff2.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
-  onoff2.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
-  onoff2.SetAttribute ("DataRate", StringValue ("2kbps"));
-  onoff2.SetAttribute ("PacketSize", UintegerValue (50));
+  //
+  // Create a UdpEchoClient application to send UDP datagrams from node zero to
+  // node one.
+  //
+  uint32_t packetSize = 1024;
+  Time interPacketInterval = Seconds (1.0);
 
-  ApplicationContainer apps2 = onoff2.Install (c.Get (1));
-  apps2.Start (Seconds (11.0));
-  apps2.Stop (Seconds (16.0));
-
-  // Create an optional packet sink to receive these packets
-  PacketSinkHelper sink ("ns3::UdpSocketFactory",
-                         Address (InetSocketAddress (Ipv4Address::GetAny (), port)));
-  apps = sink.Install (c.Get (6));
-  apps.Start (Seconds (1.0));
-  apps.Stop (Seconds (10.0));
-
-  PacketSinkHelper sink2 ("ns3::UdpSocketFactory",
-                          Address (InetSocketAddress (Ipv4Address::GetAny (), port)));
-  apps2 = sink2.Install (c.Get (6));
-  apps2.Start (Seconds (11.0));
-  apps2.Stop (Seconds (16.0));
-
+  UdpEchoClientHelper client (iic4.GetAddress(1), port);
+  client.SetAttribute ("Interval", TimeValue (interPacketInterval));
+  client.SetAttribute ("PacketSize", UintegerValue (packetSize));
+  apps = client.Install (src);
+  apps.Start (Seconds (2.0));
+  apps.Stop (Seconds (simulationTime - 20.0));
 
   AsciiTraceHelper ascii;
-  Ptr<OutputStreamWrapper> stream = ascii.CreateFileStream ("ospf_tp1.tr");
-  p2p.EnableAsciiAll (stream);
-  csma.EnableAsciiAll (stream);
-  internet.EnableAsciiIpv4All (stream);
+  csma.EnableAsciiAll (ascii.CreateFileStream ("tp1-ospf.tr"));
+  csma.EnablePcapAll ("tp1-ospf", true);
 
-  p2p.EnablePcapAll ("ospf_tp1");
-  csma.EnablePcapAll ("ospf_tp1", false);
- 
-  Ptr<Node> n1 = c.Get (1);
-  Ptr<Ipv4> ipv41 = n1->GetObject<Ipv4> ();
-  // The first ifIndex is 0 for loopback, then the first p2p is numbered 1,
-  // then the next p2p is numbered 2
-  uint32_t ipv4ifIndex1 = 2;
-
-  Simulator::Schedule (Seconds (2),&Ipv4::SetDown,ipv41, ipv4ifIndex1);
-  Simulator::Schedule (Seconds (4),&Ipv4::SetUp,ipv41, ipv4ifIndex1);
-
-  Ptr<Node> n6 = c.Get (6);
-  Ptr<Ipv4> ipv46 = n6->GetObject<Ipv4> ();
-  // The first ifIndex is 0 for loopback, then the first p2p is numbered 1,
-  // then the next p2p is numbered 2
-  uint32_t ipv4ifIndex6 = 2;
-  Simulator::Schedule (Seconds (6),&Ipv4::SetDown,ipv46, ipv4ifIndex6);
-  Simulator::Schedule (Seconds (8),&Ipv4::SetUp,ipv46, ipv4ifIndex6);
-
-  Simulator::Schedule (Seconds (12),&Ipv4::SetDown,ipv41, ipv4ifIndex1);
-  Simulator::Schedule (Seconds (14),&Ipv4::SetUp,ipv41, ipv4ifIndex1);
-
-  // Trace routing tables 
-  Ipv4GlobalRoutingHelper g;
-  Ptr<OutputStreamWrapper> routingStream = Create<OutputStreamWrapper> ("ospf_tp1.routes", std::ios::out);
-  g.PrintRoutingTableAllAt (Seconds (12), routingStream);
-
-  // Adicionado
-  /* NS_LOG_WARN ("Configuring Animation.");
+  NS_LOG_INFO ("Configuring Animation.");
 
   MobilityHelper mobility;
   mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
   mobility.Install (nodes);
-  mobility.Install (routers); */
+  mobility.Install (routers);
 
-  // Adicionado
-  /* AnimationInterface anim ("exemplo-rip.anim.xml");
+  AnimationInterface anim ("tp1-ospf.anim.xml");
   anim.SetConstantPosition(src, 10.0, 10.0); //for node src
   anim.SetConstantPosition(a, 20.0, 10.0); //for router a
   anim.SetConstantPosition(b, 30.0, 10.0); //for router b
-  anim.SetConstantPosition(dst, 40.0, 10.0); //for node dst */
-  // Adicionado
-  /* anim.UpdateNodeDescription(0, "src");
-  anim.UpdateNodeDescription(1, "dst");
-  anim.UpdateNodeDescription(2, "a");
-  anim.UpdateNodeDescription(3, "b"); */
+  anim.SetConstantPosition(c, 40.0, 10.0); //for router b
+  anim.SetConstantPosition(dst, 50.0, 10.0); //for node dst
 
-  NS_LOG_WARN ("Run Simulation.");
+  anim.UpdateNodeDescription(0, "Host_T");
+  anim.UpdateNodeDescription(1, "Host_R");
+  anim.UpdateNodeDescription(2, "Router_A");
+  anim.UpdateNodeDescription(3, "Router_B");
+  anim.UpdateNodeDescription(4, "Router_C");
+
+  // Trace routing tables 
+  // Nao sei dizer se fica, ele [e parecido com o  Ptr<OutputStreamWrapper> routingStream = Create<OutputStreamWrapper> (&std::cout);
+  Ipv4GlobalRoutingHelper g;
+  Ptr<OutputStreamWrapper> routingStream = Create<OutputStreamWrapper> ("ospf_tp1.routes", std::ios::out);
+  g.PrintRoutingTableAllAt (Seconds (12), routingStream);
+
+  NS_LOG_INFO ("Run Simulation.");
+  Simulator::Stop (Seconds(simulationTime)); // parar a simulação após 131 segundos
   Simulator::Run ();
   Simulator::Destroy ();
-  NS_LOG_WARN ("Done.");
+  NS_LOG_INFO ("Done.");
 }
